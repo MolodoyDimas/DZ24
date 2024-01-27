b005e0a0f88a7c8a4a42cbc2433e809ce260cdd6
Dmitry <kip@gmail.com>
2024-01-27 22:11:07 +0700

    исправления

--- training/serializers.py
+++ training/serializers.py
@@ -21,16 +21,17 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
+        #fields = ['name_course','description','payments','lesson_count','lessons']
 
     def get_lesson_count(self, obj):
         return obj.lesson_set.count()
 
-    def create(self, validated_data):
-        payments = validated_data.pop('payments')
-
-        course_item = Course.objects.create(**validated_data)
-
-        for x in payments:
-            Payments.objects.create(**x, course=course_item)
-
-        return course_item
+    # def create(self, validated_data):
+    #     payments_data = validated_data.pop('payments')
+    #
+    #     course_item = Course.objects.create(**validated_data)
+    #
+    #     for payment_data in payments_data:
+    #         Payments.objects.create(**payment_data, course=course_item)
+    #
+    #     return course_item
