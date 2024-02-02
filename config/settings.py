d34bc6323742da570bcff9549d6c84b518cfa966
Dmitry <kip@gmail.com>
2024-02-02 15:34:01 +0700

    добавлена проверка активности

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -169,3 +170,11 @@ EMAIL_PORT = 465
 EMAIL_HOST_USER = 'DmitryIsBack@yandex.ru'
 EMAIL_HOST_PASSWORD = 'plwhzghhersartnq'
 EMAIL_USE_SSL = True
+
+# Запускает проверку активности
+CELERY_BEAT_SCHEDULE = {
+    'update_user_activity': {
+        'task': 'training.tasks.check_update',
+        'schedule': datetime.timedelta(days=1),
+    },
+}
